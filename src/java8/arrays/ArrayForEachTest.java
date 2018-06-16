d4bc2914712c7ee126dfee7d61958d613148fdd9
Ivson Santos <santosivson@hotmail.com>
2018-06-16 10:24:38 -0300

    Pacote Java 8 criado

---
+++ src/java8/arrays/ArrayForEachTest.java
@@ -0,0 +1,19 @@
+package java8.arrays;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayForEachTest {
+
+	public static void main(String args[]) {
+		List<String> names = new ArrayList<>();
+
+		names.add("Mahesh");
+		names.add("Suresh");
+		names.add("Ramesh");
+		names.add("Naresh");
+		names.add("Kalpesh");
+
+		names.forEach(System.out::println);
+	}
+}
