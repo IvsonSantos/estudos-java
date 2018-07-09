b76cdc81fa77541e3ec8c67f92aaaa64c5103b70
Ivson Santos <santosivson@hotmail.com>
2018-07-09 20:28:08 -0300

    Classes deitel

---
+++ src/java7/ternario/TestaCondicional.java
@@ -0,0 +1,11 @@
+package java7.ternario;
+
+public class TestaCondicional {
+
+	public static void main(String[] args) {
+		
+		int a = 10;
+		
+		System.out.println(a > 5 ? "maior" : "menor");
+	}
+}
