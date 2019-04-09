b9b7ec553c467d8d71c66f80e1e2b72188a5ec98
Ivson Santos <ivson.java@gmail.com>
2019-04-09 20:39:38 -0300

    Predicate

---
+++ src/java8/consumer/predicate/PredicateTest.java
@@ -0,0 +1,48 @@
+package java8.consumer.predicate;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+import java8.consumer.Usuario;
+
+public class PredicateTest {
+
+	public static void main(String[] args) {
+
+		Usuario user1 = new Usuario("Paulo Silveira", 150);
+		Usuario user2 = new Usuario("Rodrigo Turini", 120);
+		Usuario user3 = new Usuario("Guilherme Silveira", 190);
+
+		/*
+		 * Há um detalhe aqui: o removeIf invoca o remove de uma coleção, então ela não
+		 * pode ser imutável, ou um UnsupportedOperationException será lançado. Esse é o
+		 * caso da lista devolvida por Arrays.asList
+		 */
+		// List<Usuario> usuarios = Arrays.asList(user1, user2, user3);
+		List<Usuario> usuarios = new ArrayList<>();
+		usuarios.add(user1);
+		usuarios.add(user2);
+		usuarios.add(user3);
+
+		Predicate<Usuario> predicado = new Predicate<Usuario>() {
+			@Override
+			public boolean test(Usuario t) {
+				return t.getPontos() > 160;
+			}
+		};
+
+		usuarios.removeIf(predicado);
+		usuarios.forEach(u -> System.out.println(u.getNome()));
+
+		/**
+		 * Podemos também usar aqui o lambda. Não precisamos nem mesmo declarar a
+		 * variável predicado! Podemos passar o u.getPontos() > 160 direto para o
+		 * removeIf:
+		 */
+		usuarios.removeIf(u -> u.getPontos() > 160);
+		usuarios.forEach(u -> System.out.println(u.getNome()));
+		
+	}
+
+}
